96e3346847a6228578ef1138cd8aef5549213b78
pedrofenti <pedro.fernandez@enti.cat>
2020-11-16 10:00:35 +0100

    Condiciones

--- condiciones.sh
+++ condiciones.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-cowsay "Ejercicio de condiciones"
+echo ""
+echo "-------------------------------------------"
+echo "COMPROBADOR DE EXISTENCIAS DE ARCHIVOS v0.2"
+echo "-------------------------------------------"
+echo ""
+echo "Introduce el nombre del archivo a comprobar: "
+read ARCHIVO
 
-if [ "ola" == "ola" ]; then
-	echo "Es igual"
+if [ -e $ARCHIVO ]; then
+	echo "El archivo exite"
+else
+	echo "El archivo no existe"
 fi
