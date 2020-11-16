31b682722ca87e896028fe7f5374fb9522502894
pedrofenti <pedro.fernandez@enti.cat>
2020-11-16 11:39:27 +0100

    colores con comando echo

--- bucles.sh
+++ bucles.sh
@@ -2,10 +2,23 @@
 
 echo "Ejercicio de bucles"
 
-for VARIABLE in `ls`; do 
+for VARIABLE in archivo.sh archivo bucles.sh license LICENSE; do 
 	if [ -e $VARIABLE ]; then
-		echo "$VARIABLE existe"
+		echo -e "\e[92m $VARIABLE \e[0m existe"
 	else 
-		echo "$VARIABLE no existe"
+		echo -e "\e[91m $VARIABLE \e[0m no existe"
 	fi
+done
+
+echo ""
+echo  "ARCOIRIS"
+
+for VARIABLE in A R C O I R I S; do
+	echo -e "\e[31m $VARIABLE \e[0m"
+	echo -e "\e[32m $VARIABLE \e[0m"
+	echo -e "\e[33m $VARIABLE \e[0m"
+	echo -e "\e[34m $VARIABLE \e[0m"
+	echo -e "\e[35m $VARIABLE \e[0m"
+	echo -e "\e[36m $VARIABLE \e[0m"
+	echo -e "\e[37m $VARIABLE \e[0m"
 done
