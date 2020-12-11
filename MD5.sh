9e451d88b8e74f39afb4e2b18b394a5dd5ccd2c5
pedrofenti <pedro.fernandez@enti.cat>
2020-12-11 11:42:26 +0100

    configuracion de .vimrc

--- MD5.sh
+++ MD5.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-echo "Ejercicio   "
+echo "Ejercicio  Bucles y MD5"
+sleep 1
+
+EXTENSION="mp3"
 
 for ARCHIVO in `ls *.mkv`; do
 	EJECUTABLE=`file $ARCHIVO | grep -i media`
 	echo $EJECUTABLE
+	
 	if [ "$ARCHIVO" != "" ]; then
 		#md5sum $VARIABLE
+		
+		echo "Convirtiendo $ARCHIVO a $EXTENSION"
+		sleep 3
+
 		NUEVONOMBRE=`echo $ARCHIVO | cut -d "." -f 1`
-		ffmpeg -i $ARCHIVO $NUEVONOMBRE.mp4
+		ffmpeg -i $ARCHIVO $NUEVONOMBRE.$EXTENSION
 	fi
+	clear
+
 done
 	
